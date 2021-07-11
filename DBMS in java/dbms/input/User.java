caaca3605e99efc7604983342837f44964358749
sayandey-dev <sayandeyip@gmail.com>
2021-07-11 18:27:51 +0530

    Added Functions

--- DBMS in java/dbms/input/User.java
+++ DBMS in java/dbms/input/User.java
@@ -5,14 +5,22 @@ public class User{
     public static boolean isv;
     public static String takeUserInput(){
         Scanner sc = new Scanner(System.in);
-        st = sc.next();
+        st = sc.nextLine();
         return st;
     }
 
     public static boolean isValid(String s){
-        
+        if(s.substring(0, 7) == "create "){
+            System.out.println("SUCCESS");
+        }
 
         return isv;
+    }
+
+    public static String returnTableName(String str){
+        String table_name;
+        table_name = str.substring(14);
+        return table_name;
     }
 
 
@@ -20,5 +28,12 @@ public class User{
         String str;
         str = User.takeUserInput();
         System.out.println(str);
+
+        //String s = str.substring(0,6);
+        //System.out.println(s);
+        //User.isValid(str);
+        boolean b = User.isValid(str);
+        System.out.println(b);
+
     }
 }
